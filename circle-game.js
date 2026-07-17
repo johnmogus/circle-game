eb579149cf761163d54cfba3dfe48ed4e7894e1c
diddydooright <111104070+johnmogus@users.noreply.github.com>
2026-07-17 01:31:20 -0500

    gibs are added now i just need realistic water physics and maybe a level system at some point

--- circle-game.js
+++ circle-game.js
@@ -21,11 +21,21 @@ let gameOver = false;
 function randInt(min, max) {
     return Math.round((Math.random()*(max-min))+min)
 }
+function rand(min, max) {
+    return (Math.random()*(max-min))+min;
+}
 
 function drawFilledCircle(x, y, radius, color) {
     ctx.fillStyle = color;
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
+}
+function drawFilledArc(x, y, radius, color, startAngle, endAngle) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle);
+    ctx.lineTo(x, y);
     ctx.fill();
 }
 
@@ -98,7 +108,7 @@ class SpiralPattern extends ShootPattern {
             var angle = (this.shotNumber / this.shotsPerCycle)*(Math.PI * 2);
             var vx = Math.cos(angle) * this.speed;
             var vy = Math.sin(angle) * this.speed;
-            entities.bullets.push(new EnemyBullet(x, y, vx, vy, this.radius));
+            bullets.push(new EnemyBullet(x, y, vx, vy, this.radius, this.damage));
             this.shotNumber++;
             this.cooldownTimer = this.shotCooldown;
         }
@@ -118,7 +128,7 @@ class BurstRingPattern extends ShootPattern {
                 var angle = (i / this.bulletAmount) * Math.PI * 2;
                 var vx = Math.cos(angle) * this.speed;
                 var vy = Math.sin(angle) * this.speed;
-                entities.bullets.push(new EnemyBullet(x, y, vx, vy, this.radius));
+                bullets.push(new EnemyBullet(x, y, vx, vy, this.radius, this.damage));
             }
             this.cooldownTimer = this.shotCooldown;
         }
@@ -134,7 +144,7 @@ class TowardsPlayerPattern extends ShootPattern {
         else {
             var closestPlayerX = null;
             var closestPlayerY = null;
-            for (var p of entities.players) {
+            for (var p of players) {
                 var dist = Math.hypot(x - p.x, y - p.y);
                 if (closestPlayerX == null || closestPlayerY == null) {
                     closestPlayerX = p.x;
@@ -150,7 +160,7 @@ class TowardsPlayerPattern extends ShootPattern {
             var angle = Math.atan2(closestPlayerX - x, closestPlayerY - y);
             var vx = Math.cos(angle - Math.PI/2) * this.speed;
             var vy = Math.sin(angle + Math.PI/2) * this.speed;
-            entities.bullets.push(new EnemyBullet(x, y, vx, vy, this.radius));
+            bullets.push(new EnemyBullet(x, y, vx, vy, this.radius, this.damage));
             this.shotCooldownTimer = this.shotCooldown;
         }
     }
@@ -165,60 +175,78 @@ class Entity {
     }
     step() {}
     hit() {}
-    render() {}
+    entityRender() {}
+    uiRender() {}
+}
+
+function updateEntities(list, dt) {
+    for (var i = 0; i < list.length; i++) {
+        var e = list[i];
+        e.step(dt);
+        if (e.health <= 0) {
+            gibs.push(new GibSpawner(e.x, e.y, e.radius, e.color, 12))
+            list.splice(i, 1);
+        }
+    }
 }
 
 class Bullet extends Entity {
-    constructor(x, y, vx, vy, radius) {
+    constructor(x, y, vx, vy, radius, damage) {
         super(x, y, 1);
         this.vx = vx;
         this.vy = vy;
         this.radius = radius;
+        this.damage = damage;
         this.color = '#ffffff'
     }
     step(dt) {
-        this.x += this.vx * dt;
-        this.y += this.vy * dt;
-
         if (this.x < -10 || this.x > canvas.width + 10 || this.y < -10 || this.y > canvas.height + 10) {
             this.health = 0;
         }
     }
-    render() {
+    entityRender() {
         drawFilledCircle(this.x, this.y, this.radius, this.color)
     }
 }
 
 class PlayerBullet extends Bullet {
-    constructor(x, y, vx, vy, radius) {
-        super(x, y, vx, vy, radius);
+    constructor(x, y, vx, vy, radius, damage) {
+        super(x, y, vx, vy, radius, damage);
         this.color = '#00a5f1'
     }
     step(dt) {
-        for (var e of entities.enemies) {
+        for (var e of enemies) {
             var dist = Math.hypot(this.x - e.x, this.y - e.y);
             if (dist < this.radius + e.hitboxRadius) {
-                e.hit();
+                e.hit(this.damage);
                 this.health = 0;
             }
         }
+
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+
         super.step(dt);
     }
 }
 
 class EnemyBullet extends Bullet {
-    constructor(x, y, vx, vy, radius) {
-        super(x, y, vx, vy, radius);
+    constructor(x, y, vx, vy, radius, damage) {
+        super(x, y, vx, vy, radius, damage);
         this.color = '#ff3336'
     }
     step(dt) {
-        for (var p of entities.players) {
+        for (var p of players) {
             var dist = Math.hypot(this.x - p.x, this.y - p.y);
-            if (dist < this.radius + p.hitboxRadius) {
-                p.hit();
+            if (dist < this.radius + p.hitboxRadius && p.hitCooldownTimer <= 0) {
+                p.hit(this.damage);
                 this.health = 0;
             }
         }
+
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+
         super.step(dt);
     }
 }
@@ -232,7 +260,7 @@ class Player extends Entity {
         this.radius = 15;
         this.hitboxRadius = 5;
         this.moveSpeed = 400;
-        this.damage = 10;
+        this.bulletDamage = 10;
         this.shotCooldown = 0.1;
         this.shotCooldownTimer = 0;
         this.hitCooldown = 1;
@@ -252,11 +280,18 @@ class Player extends Entity {
         // Player bullets
         if (this.shotCooldownTimer > 0) this.shotCooldownTimer -= dt;
         else if (keys['z']) {
-            entities.bullets.push(new PlayerBullet(this.x, this.y, 0, -500, 5));
+            bullets.push(new PlayerBullet(this.x, this.y, 0, -500, 6, this.bulletDamage));
             this.shotCooldownTimer = this.shotCooldown;
         }
 
-        if (this.hitCooldownTimer > 0) {
+        if (this.health <= 0 && this.hitCooldownTimer <= 0) {
+            this.x = this.firstX;
+            this.y = this.firstY;
+            this.hitCooldownTimer = this.hitCooldown;
+            this.lives--;
+            this.health = 1;
+        }
+        else if (this.hitCooldownTimer > 0) {
             this.color = 'rgba(0, 255, 204, 0.5)';
             this.hitCooldownTimer -= dt;
         }
@@ -265,19 +300,20 @@ class Player extends Entity {
         }
 
     }
-    hit() {
-        if (this.hitCooldownTimer <= 0) {
-            this.x = this.firstX;
-            this.y = this.firstY;
-            this.hitCooldownTimer = this.hitCooldown;
-            this.lives--;
-        }
+    hit(damage) {
+        this.health -= damage;
     }
-    render() {
+    entityRender() {
         //body
         drawFilledCircle(this.x, this.y, this.radius, this.color)
         //hitbox
         drawFilledCircle(this.x, this.y, this.hitboxRadius, 'red')
+    }
+    uiRender() {
+        ctx.textAlign = "center"; 
+        ctx.fillStyle = 'white';
+        ctx.font = "25px Arial"
+        ctx.fillText("Lives: " + Math.floor(players[0].lives), 500, 25);
     }
 }
 
@@ -290,6 +326,9 @@ class Enemy extends Entity {
         for (var p of this.shootPatterns) {
             p.step(this.x, this.y, dt);
         }
+    }
+    hit(damage) {
+        this.health -= damage;
     }
 }
 
@@ -310,13 +349,14 @@ class Boss1 extends Enemy {
     step(dt) {
         super.step(dt);
     }
-    render() {
+    entityRender() {
+        drawFilledCircle(this.x, this.y, this.radius, this.color)
+    }
+    uiRender() {
         ctx.font = "25px Arial";
         ctx.fillStyle = 'white';
         ctx.textAlign = "start"; 
         ctx.fillText("Boss Health: " + this.health, 10, 25);
-
-        drawFilledCircle(this.x, this.y, this.radius, this.color)
     }
 }
 
@@ -336,17 +376,71 @@ class SideSniper extends Enemy {
         this.y += this.moveSpeed * dt;
         super.step(dt);
     }
-    render() {
+    entityRender() {
         drawFilledCircle(this.x, this.y, this.radius, this.color)
     }
 }
 
+class Gib extends Entity {
+    constructor(x, y, vx, vy, radius, color, startAngle, endAngle, timer) {
+        super(x, y, 1)
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = radius;
+        this.color = color;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.timer = timer;
+    }
+    step(dt) {
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+        if (this.timer > 0) this.timer -= dt;
+        else {
+            this.health = 0;
+        }
+    }
+    entityRender() {
+        drawFilledArc(this.x, this.y, this.radius, this.color, this.startAngle, this.endAngle)
+    }
+}
 
-const entities = {
-    players: [new Player(300, 700)],
-    enemies: [new Boss1(300, 200)],
-    bullets: []
-};
+class GibSpawner extends Entity{
+    constructor(x, y, radius, color, amount) {
+        super(x, y, 1);
+        this.gibs = [];
+        for (var i = 0; i < amount; i++) {
+            var timer = rand(1, 2);
+            var startAngle = (i / amount) * (Math.PI * 2);
+            var endAngle = ((i+1) / amount) * (Math.PI * 2);
+            var moveAngle = startAngle+((endAngle-startAngle)/2);
+            var speed = randInt(35, 50);
+            var vx = Math.cos(moveAngle) * speed;
+            var vy = Math.sin(moveAngle) * speed;
+            this.gibs.push(new Gib(this.x, this.y, vx, vy, radius, color, startAngle, endAngle, timer));
+        }
+    }
+    step(dt) {
+        for (var i = 0; i < this.gibs.length; i++) {
+            var g = this.gibs[i];
+            g.step(dt);
+            if (g.health <= 0) {
+                this.gibs.splice(i, 1);
+            }
+        }
+    }
+    entityRender() {
+        for (var g of this.gibs) {
+            g.entityRender();
+        }
+    }
+}
+
+
+const players = [new Player(300, 700)];
+const enemies = [new Boss1(300, 200)];
+const bullets = [];
+const gibs = [];
 
 
 function loop() {
@@ -368,15 +462,18 @@ function loop() {
 function update(dt) {
     if (keys['Escape']) gamePaused = true;
     if (gamePaused) {
-        ctx.fillStyle = 'red';
-        ctx.textAlign = "center"; 
-        ctx.font = "25px Arial";
         if (gameOver) {
             if (keys["Enter"]) window.location.reload();
+            ctx.fillStyle = 'red';
+            ctx.textAlign = "center"; 
+            ctx.font = "25px Arial";
             ctx.fillText(`Final Score: ${Math.floor(score)}`, 300, 400);
         }
         else {
             if (keys["Enter"]) gamePaused = false;
+            ctx.fillStyle = 'red';
+            ctx.textAlign = "center"; 
+            ctx.font = "25px Arial";
             ctx.fillText("Score: " + Math.floor(score), 300, 400);
         }
         return;
@@ -384,34 +481,22 @@ function update(dt) {
 
     score += dt * 10;
 
-    for (var i = 0; i < entities.players.length; i++) {
-        var p = entities.players[i];
+    for (var i = 0; i < players.length; i++) {
+        var p = players[i];
         p.step(dt);
         if (p.lives <= 0) {
-            entities.players.splice(i, 1);
+            players.splice(i, 1);
         }
     }
-    if (entities.players.length <= 0) {
+    if (players.length <= 0) {
         gamePaused = true;
         gameOver = true;
         return;
     }
 
-    for (var i = 0; i < entities.enemies.length; i++) {
-        var e = entities.enemies[i];
-        e.step(dt);
-        if (e.health <= 0) {
-            entities.enemies.splice(i, 1);
-        }
-    }
-    
-    for (var i = 0; i < entities.bullets.length; i++) {
-        var b = entities.bullets[i];
-        b.step(dt);
-        if (b.health <= 0) {
-            entities.bullets.splice(i, 1);
-        }
-    }
+    updateEntities(enemies, dt);
+    updateEntities(bullets, dt);
+    updateEntities(gibs, dt);
 
     //kill
     // if (e.y > canvas.height + e.size || e.health <= 0) {
@@ -426,25 +511,40 @@ function update(dt) {
 
     //Draw
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Draw Entities
+    for (var p of players) {
+        p.entityRender();
+    }
+    for (var e of enemies) {
+        e.entityRender();
+    }
+    for (var b of bullets) {
+        b.entityRender();
+    }
+    for (var g of gibs) {
+        g.entityRender();
+    }
+
+    //Score and Other Stuff
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(0, 0, canvas.width, 40)
+
+    for (var p of players) {
+        p.uiRender();
+    }
+    for (var e of enemies) {
+        e.uiRender();
+    }
+
     ctx.textAlign = "center"; 
     ctx.fillStyle = 'white';
     ctx.font = "25px Arial"
     ctx.fillText("Score: " + Math.floor(score), 300, 25);
-    ctx.fillText("Health: " + Math.floor(entities.players[0].lives), 500, 25);
+
     // ctx.font = "10px Arial";
-    // ctx.fillText("bullets " + entities.bullets.length, 10, 50);
-    // ctx.fillText("enemies " + entities.enemies.length, 10, 60);
-    
-    // Draw Entities
-    for (var p of entities.players) {
-        p.render();
-    }
-    for (var e of entities.enemies) {
-        e.render();
-    }
-    for (var b of entities.bullets) {
-        b.render();
-    }
+    // ctx.fillText("bullets " + bullets.length, 10, 50);
+    // ctx.fillText("enemies " + enemies.length, 10, 60);
 
 }
 
